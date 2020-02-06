3f149f724825df4dc1466a2e82bbae18f9e3f98f
Mindy Tchieu <mtchieu@gmail.com>
2020-02-06 17:23:16 -0500

    Fix import and move files into folders

--- src/components/MainNav/MainNav.stories.js
+++ src/components/MainNav/MainNav.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
-import MainNav from "./main-nav.jsx";
+import MainNav from "./MainNav.jsx";
 
 storiesOf("MainNav", module)
   .add("default", () => <MainNav locale="en-US" />)
