9702d519f7884437efadf9076361201dbdb2593f
Championrunner <ishaanmalikrm7@gmail.com>
2020-09-10 20:34:36 +0530

    Update addons.js

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,15 @@
-import '@storybook/addon-actions/register';
-import '@storybook/addon-links/register';
+import "@storybook/addon-actions/register";
+import "@storybook/addon-links/register";
+import "@storybook/addons";
+import { linkTo } from "@storybook/addon-links";
+
+export default {
+  title: "Button",
+};
+
+export const first = () => (
+  <button onClick={linkTo("Button", "second")}>Go to "Second"</button>
+);
+export const second = () => (
+  <button onClick={linkTo("Button", "first")}>Go to "First"</button>
+);
